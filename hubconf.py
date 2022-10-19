2bc285fd90355e60366f378ff3dc8d531683ac40
Nitesh MGS <70936290+cs19b028iittp@users.noreply.github.com>
2022-10-19 09:25:58 +0530

    Update hubconf.py

---
+++ hubconf.py
@@ -0,0 +1,7 @@
+class cs19028(nn.Module):
+  def __init__(self):
+        super(cs19b028NN, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(),
+        )
