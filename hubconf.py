4724357baeccbddaa5d352bc280563b8079a812d
Nitesh MGS <70936290+cs19b028iittp@users.noreply.github.com>
2022-10-19 10:33:28 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -58,93 +58,93 @@ class cs19b028NN(nn.Module):
     return train_dataloader, test_dataloader
 
   # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
-def get_model(train_data_loader, n_epochs):
-  model = cs19b028NN().to(device)
-  
-  return model
+  def get_model(train_data_loader, n_epochs):
+    model = cs19b028NN().to(device)
+    
+    return model
 
-training_data, test_data = load_data();
-train_dataloader, test_dataloader = create_dataloaders(training_data, test_data, batch_size=64)
+  training_data, test_data = load_data();
+  train_dataloader, test_dataloader = create_dataloaders(training_data, test_data, batch_size=64)
 
-def get_model_advanced(train_data_loader=train_dataloader, n_epochs=10,lr=1e-4,config=None):
-  model = get_model();
-  
-  # To train a model, we need a loss function and an optimizer.
-  optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+  def get_model_advanced(train_data_loader=train_dataloader, n_epochs=10,lr=1e-4,config=None):
+    model = get_model();
+    
+    # To train a model, we need a loss function and an optimizer.
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-  size = len(train_dataloader.dataset)
-  model.train()
-  for batch, (X, y) in enumerate(train_dataloader):
-      X, y = X.to(device), y.to(device)
+    size = len(train_dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(train_dataloader):
+        X, y = X.to(device), y.to(device)
 
-      # Compute prediction error
-      pred = model(X)
-      loss = loss_fn(pred, y)
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
 
-      # Backpropagation
-      optimizer.zero_grad()
-      loss.backward()
-      optimizer.step()
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-      if batch % 100 == 0:
-          loss, current = loss.item(), batch * len(X)
-          print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-  return model
-
-
-  def test_model(model1, test_data_loader,force_reload=True):
-
-  accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
-
-  size = len(test_data_loader.dataset)
-  num_batches = len(test_data_loader)
-  model1.eval()
-  test_loss, correct = 0, 0
-  with torch.no_grad():
-      for X, y in test_data_loader:
-          X, y = X.to(device), y.to(device)
-          pred = model1(X)
-          test_loss += loss_fn(pred, y).item()
-          correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-  test_loss /= num_batches
-  correct /= size
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    return model
 
 
-  accuracy_val = correct
+    def test_model(model1, test_data_loader,force_reload=True):
 
-  classes = [
-    "BMW",
-    "Mecedes",
-    "Porche",
-    "Ducati",
-    "Cullinen",
-    "Suzuki",
-    "Skoda",
-    "Volvo",
-    "Bentley",
-    "Audi",
-  ]
+      accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
 
-  model = model1.eval()
-  x = []
-  y = [] 
-  y1 = []
-  for i in test_data :
-    x.append(i[0])
-    y.append(i[1])
-    with torch.no_grad():
-      pred = model(i[0])
-      predicted = pred[0].argmax(0)
-      # print()
-      y1.append(classes.index(classes[predicted]))
-  
-  print ('Returning metrics... (rollnumber: cs19b028)')
-  print(y)
-  print(y1)
-  precision_recall_fscore_support(y, y1, average='macro')
-  
-  return accuracy_val, precision_val, recall_val, f1score_val
+      size = len(test_data_loader.dataset)
+      num_batches = len(test_data_loader)
+      model1.eval()
+      test_loss, correct = 0, 0
+      with torch.no_grad():
+          for X, y in test_data_loader:
+              X, y = X.to(device), y.to(device)
+              pred = model1(X)
+              test_loss += loss_fn(pred, y).item()
+              correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+      test_loss /= num_batches
+      correct /= size
 
-ans = test_model()
+
+      accuracy_val = correct
+
+      classes = [
+        "BMW",
+        "Mecedes",
+        "Porche",
+        "Ducati",
+        "Cullinen",
+        "Suzuki",
+        "Skoda",
+        "Volvo",
+        "Bentley",
+        "Audi",
+      ]
+
+      model = model1.eval()
+      x = []
+      y = [] 
+      y1 = []
+      for i in test_data :
+        x.append(i[0])
+        y.append(i[1])
+        with torch.no_grad():
+          pred = model(i[0])
+          predicted = pred[0].argmax(0)
+          # print()
+          y1.append(classes.index(classes[predicted]))
+      
+      print ('Returning metrics... (rollnumber: cs19b028)')
+      print(y)
+      print(y1)
+      precision_recall_fscore_support(y, y1, average='macro')
+      
+      return accuracy_val, precision_val, recall_val, f1score_val
+
+    ans = test_model()
 
 
