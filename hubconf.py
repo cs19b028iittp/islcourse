c4b62f3508b3b062943c6c3e0078921d24ecd852
Nitesh MGS <70936290+cs19b028iittp@users.noreply.github.com>
2022-11-21 10:44:52 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -5,6 +5,11 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics.cluster import homogeneity_score, completeness_score, v_measure_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, roc_auc_score
+
 
 ###### PART 1 ######
 
@@ -13,7 +18,7 @@ def get_data_blobs(n_points=100):
   return X,y
 
 def get_data_circles(n_points=100):
-  X, y = make_circles(n_samples=n_points, shuffle=True,  factor=0.7, random_state=0)
+  X, y = make_circles(n_samples=n_points, shuffle=True,  factor=0.8, random_state=0)
   return X,y
 
 def get_data_mnist():
@@ -36,3 +41,26 @@ def compare_clusterings(ypred_1=None,ypred_2=None):
   c=completeness_score(ypred_1,ypred_2)
   v=v_measure_score(ypred_1,ypred_2)
   return h,c,v
+
+
+###### PART 2 ######
+
+def build_lr_model(X=None, y=None):
+  lr_model = LogisticRegression(random_state=0)
+  lr_model.fit(X,y)
+  return lr_model
+
+def build_rf_model(X=None, y=None):
+  rf_model = RandomForestClassifier(max_depth=5, random_state=0)
+  rf_model.fit(X,y)
+  return rf_model
+
+def get_metrics(model=None,X=None,y=None):
+  acc, prec, rec, f1, auc = 0,0,0,0,0
+  y_pred = model.predict(X)
+  acc = accuracy_score(y,y_pred)
+  prec=precision_score(y, ypred,average='macro')
+  rec=recall_score(y, ypred,average='macro')
+  f1=f1_score(y, ypred,average='macro')
+  auc = roc_auc_score(y,y_pred,average='macro',multi_class='ovr')
+  return acc, prec, rec, f1, auc
