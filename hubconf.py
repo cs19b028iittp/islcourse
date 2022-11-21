1b4935680d54b6de5b29a7b79632bd9098b0d53e
Nitesh MGS <70936290+cs19b028iittp@users.noreply.github.com>
2022-11-21 12:17:59 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -104,23 +104,25 @@ def perform_gridsearch_cv_multimetric(model1=None, param_grid=None, cv=5, X=None
      # pass
   #return top1_scores
 
+
+  
+  
 class MyNN(nn.Module):
   def __init__(self,inp_dim=64,hid_dim=13,num_classes=10):
     super(MyNN,self).__init__()
-
-    self.inp_dim = inp_dim
-    self.hid_dim = hid_dim
-    self.num_classes = num_classes
     
-    self.fc_encoder = None # write your code inp_dim to hid_dim mapper
-    self.fc_decoder = None # write your code hid_dim to inp_dim mapper
-    self.fc_classifier = None # write your code to map hid_dim to num_classes
+    self.flatten = nn.Flatten()
+    self.fc_encoder = nn.Linear(in_features=inp_dim, out_features=hid_dim) # write your code inp_dim to hid_dim mapper
+    self.fc_decoder = nn.Linear(in_features=hid_dim, out_features=inp_dim) # write your code hid_dim to inp_dim mapper
+    self.fc_classifier = nn.Linear(in_features=hid_dim, out_features=num_classes) # write your code to map hid_dim to num_classes
     
-    self.relu = None #write your code - relu object
-    self.softmax = None #write your code - softmax object
+    self.relu = nn.ReLU() #write your code - relu object
+    self.softmax = nn.Softmax(dim=0) #write your code - softmax object
     
   def forward(self,x):
-    x = None # write your code - flatten x
+    # x = None # write your code - flatten x
+    # x = self.flatten(x)
+    # print(x.shape)
     x_enc = self.fc_encoder(x)
     x_enc = self.relu(x_enc)
     
@@ -136,7 +138,9 @@ class MyNN(nn.Module):
     
     # class prediction loss
     # yground needs to be one hot encoded - write your code
-    lc1 = None # write your code for cross entropy between yground and y_pred, advised to use torch.mean()
+    yground = nn.Functional.one_hot(yground, num_classes=10)
+
+    lc1 = torch.mean(-torch.mul(yground, torch.log(y_pred))) # write your code for cross entropy between yground and y_pred, advised to use torch.mean()
     
     # auto encoding loss
     lc2 = torch.mean((x - xencdec)**2)
@@ -145,37 +149,48 @@ class MyNN(nn.Module):
     
     return lval
     
-def get_mynn(inp_dim=64,hid_dim=13,num_classes=10):
-  mynn = MyNN(inp_dim,hid_dim,num_classes)
-  mynn.double()
-  return mynn
+  def get_mynn(inp_dim=64,hid_dim=13,num_classes=10):
+    print("hello1")
+    mynn = MyNN(inp_dim,hid_dim,num_classes)
+    print("hello2")
+    mynn.double()
+    print("hello3")
+    return mynn
 
-def get_mnist_tensor():
-  # download sklearn mnist
-  # convert to tensor
-  X, y = get_data_mnist()
-  X, y = torch.from_numpy(X), torch.from_numpy(y)
-  # write your code
-  return X,y
+  def get_mnist_tensor():
+    # download sklearn mnist
+    # convert to tensor
 
-def get_loss_on_single_point(x0,y0,mynn=None):
-  y_pred, xencdec = mynn(x0)
-  lossval = mynn.loss_fn(x0,y0,y_pred,xencdec)
-  # the lossval should have grad_fn attribute set
-  return lossval
+    X, y = load_digits(n_class=10, return_X_y=True)
+    X = torch.from_numpy(X)
+    y = torch.from_numpy(y)
+    # print(type(X))
+    # print(type(y))
+    # write your code
+    return X,y
 
-def train_combined_encdec_predictor(X,y, mynn=None, epochs=11):
-  # X, y are provided as tensor
-  # perform training on the entire data set (no batches etc.)
-  # for each epoch, update weights
-  
-  optimizer = optim.SGD(mynn.parameters(), lr=0.01)
-  
-  for i in range(epochs):
-    optimizer.zero_grad()
-    ypred, Xencdec = mynn(X)
-    lval = mynn.loss_fn(X,y,ypred,Xencdec)
-    lval.backward()
-    optimizer.step()
-    
-  return mynn
+  def get_loss_on_single_point(mynn,x0,y0):
+    y_pred, xencdec = mynn(x0)
+    print(y_pred)
+    print(y_pred.shape)
+    print(xencdec)
+    # lossval = mynn.loss_fn(x0,y0,y_pred,xencdec)
+    lossval = 0
+    # the lossval should have grad_fn attribute set
+    return lossval
+
+  def train_combined_encdec_predictor(mynn,X,y, epochs=11):
+    # X, y are provided as tensor
+    # perform training on the entire data set (no batches etc.)
+    # for each epoch, update weights
+
+    optimizer = optim.SGD(mynn.parameters(), lr=0.01)
+
+    for i in range(epochs):
+      optimizer.zero_grad()
+      ypred, Xencdec = mynn(X)
+      lval = mynn.loss_fn(X,y,ypred,Xencdec)
+      lval.backward()
+      optimzer.step()
+
+    return 
