a908194db3be648bbde32117111a691fee03a4a9
Nitesh MGS <70936290+cs19b028iittp@users.noreply.github.com>
2022-11-21 12:02:44 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -78,19 +78,26 @@ def get_paramgrid_rf():
 
 def perform_gridsearch_cv_multimetric(model1=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
   
-  grid_search_cv = GridSearchCV(model1, param_grid, cv=cv, scoring=metrics, refit=False)
-  grid_search_cv.fit(X, y)
+ # grid_search_cv = GridSearchCV(model1, param_grid, cv=cv, scoring=metrics, refit=False)
+ # grid_search_cv.fit(X, y)
 
-  cv_results = cross_validate(model1, X, y, cv=cv, scoring=metrics)
+  #cv_results = cross_validate(model1, X, y, cv=cv, scoring=metrics)
   
-  top1_scores = [0.0 for i in range(len(metrics))]
-  for i in range(len(metrics)):
-    try:
-      top1_scores[i]=grid_search_cv.scorer_[metrics[i]](grid_search_cv,X,y)
-      print('******##$$%%%')
-    except:
-      pass
+  top1_scores = []#[0.0 for i in range(len(metrics))]
+  
+  for scoring in metrics:
+    grid_search_cv = GridSearchCV(model,param_grid, cv=cv, scoring=scoring)
+    grid_search_cv.fit(X,y)
+    top1_scores.append(grid_search_cv.best_score_)
+
   return top1_scores
+  #for i in range(len(metrics)):
+   # try:
+    #  top1_scores[i]=grid_search_cv.scorer_[metrics[i]](grid_search_cv,X,y)
+     # print('******##$$%%%')
+    #except:
+     # pass
+  #return top1_scores
 
       
  
