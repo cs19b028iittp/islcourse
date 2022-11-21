8239b2e14936b83c76e9e88d00fbfeab0c7707f1
Nitesh MGS <70936290+cs19b028iittp@users.noreply.github.com>
2022-11-21 11:29:53 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -9,6 +9,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_validate
 
 
 ###### PART 1 ######
