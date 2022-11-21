8883b648a2ce487ec2ae82d87393bae2cf177bbd
Nitesh MGS <70936290+cs19b028iittp@users.noreply.github.com>
2022-11-21 12:30:58 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -83,10 +83,10 @@ def get_paramgrid_rf():
 
 def perform_gridsearch_cv_multimetric(model1=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
   
- # grid_search_cv = GridSearchCV(model1, param_grid, cv=cv, scoring=metrics, refit=False)
- # grid_search_cv.fit(X, y)
+  grid_search_cv = GridSearchCV(model1, param_grid, cv=cv, scoring=metrics, refit=False)
+  grid_search_cv.fit(X, y)
 
-  #cv_results = cross_validate(model1, X, y, cv=cv, scoring=metrics)
+  cv_results = cross_validate(model1, X, y, cv=cv, scoring=metrics)
   
   top1_scores = []
   
