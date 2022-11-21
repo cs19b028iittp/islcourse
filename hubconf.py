e8df1ad0913b31d923b1c85c3e1a9a9a7a10f0e3
Nitesh MGS <70936290+cs19b028iittp@users.noreply.github.com>
2022-11-21 12:16:06 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -104,27 +104,28 @@ def perform_gridsearch_cv_multimetric(model1=None, param_grid=None, cv=5, X=None
      # pass
   #return top1_scores
 
-  
-  ###### PART 3 ######
-
 class MyNN(nn.Module):
   def __init__(self,inp_dim=64,hid_dim=13,num_classes=10):
     super(MyNN,self).__init__()
+
+    self.inp_dim = inp_dim
+    self.hid_dim = hid_dim
+    self.num_classes = num_classes
     
-    self.fc_encoder = nn.Linear(inp_dim,hid_dim) # write your code inp_dim to hid_dim mapper
-    self.fc_decoder = nn.Linear(hid_dim,inp_dim) # write your code hid_dim to inp_dim mapper
-    self.fc_classifier = nn.Linear(hid_dim,num_classes) # write your code to map hid_dim to num_classes
+    self.fc_encoder = None # write your code inp_dim to hid_dim mapper
+    self.fc_decoder = None # write your code hid_dim to inp_dim mapper
+    self.fc_classifier = None # write your code to map hid_dim to num_classes
     
-    self.relu = nn.ReLU #write your code - relu object
-    self.softmax = nn.Softmax #write your code - softmax object
+    self.relu = None #write your code - relu object
+    self.softmax = None #write your code - softmax object
     
   def forward(self,x):
-    x = torch.flatten(x) # write your code - flatten x
+    x = None # write your code - flatten x
     x_enc = self.fc_encoder(x)
-    x_enc = self.relu()(x_enc)
+    x_enc = self.relu(x_enc)
     
     y_pred = self.fc_classifier(x_enc)
-    y_pred = self.softmax()(y_pred)
+    y_pred = self.softmax(y_pred)
     
     x_dec = self.fc_decoder(x_enc)
     
@@ -135,8 +136,7 @@ class MyNN(nn.Module):
     
     # class prediction loss
     # yground needs to be one hot encoded - write your code
-    yground = F.one_hot(yground,num_classes=y_pred.shape[0])
-    lc1 = -torch.mean(torch.mul(yground,torch.log(y_pred+0.001))) # write your code for cross entropy between yground and y_pred, advised to use torch.mean()
+    lc1 = None # write your code for cross entropy between yground and y_pred, advised to use torch.mean()
     
     # auto encoding loss
     lc2 = torch.mean((x - xencdec)**2)
@@ -153,19 +153,18 @@ def get_mynn(inp_dim=64,hid_dim=13,num_classes=10):
 def get_mnist_tensor():
   # download sklearn mnist
   # convert to tensor
-  npX,npy = get_data_mnist()
-  X, y = torch.from_numpy(npX),torch.from_numpy(npy)
+  X, y = get_data_mnist()
+  X, y = torch.from_numpy(X), torch.from_numpy(y)
   # write your code
   return X,y
 
-def get_loss_on_single_point(mynn,x0,y0):
+def get_loss_on_single_point(x0,y0,mynn=None):
   y_pred, xencdec = mynn(x0)
   lossval = mynn.loss_fn(x0,y0,y_pred,xencdec)
-  print(lossval)
   # the lossval should have grad_fn attribute set
   return lossval
 
-def train_combined_encdec_predictor(mynn,X,y, epochs=11):
+def train_combined_encdec_predictor(X,y, mynn=None, epochs=11):
   # X, y are provided as tensor
   # perform training on the entire data set (no batches etc.)
   # for each epoch, update weights
@@ -180,6 +179,3 @@ def train_combined_encdec_predictor(mynn,X,y, epochs=11):
     optimizer.step()
     
   return mynn
-
-      
- 
